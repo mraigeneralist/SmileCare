fed3d668473159fb5c1018a86522744d83c35674
Abbas Ali <mraigeneralist@gmail.com>
2026-04-11 14:59:29 +0530

    feat: initialize Supabase client with environment variables

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://tkqqxkonbjgniznhipuj.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRrcXF4a29uYmpnbml6bmhpcHVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU4MzM3NjIsImV4cCI6MjA5MTQwOTc2Mn0.ETgpcb0hYmgzUZLmzgxOSDQX2AJJHp3y3rNyH016BFg";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
